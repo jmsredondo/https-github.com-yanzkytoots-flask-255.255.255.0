7952a1bd496e3f11641cd5fb2a36944a57aff791
ianali <yanzkytootsky@gmail.com>
2018-08-22 23:20:51 +0800

    Added registration on routes.py

--- app/routes.py
+++ app/routes.py
@@ -18,10 +18,26 @@ def login_page():
             "username": form.username.data,
             "password": form.password.data
         }
-        print userdict
+
         r = requests.post("http://localhost:80/users/login", data=userdict)
-        print r.status_code
         if r.status_code == 200:
             return render_template('Admin/dashboard.html', username=form.username.data)
         return redirect('/login#loginFailed')
     return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        reg = {
+            "username": form.username.data,
+            "password": form.username.data,
+            "firstname": form.username.data,
+            "lastname": form.username.data,
+            "phone": form.username.data,
+            "email": form.username.data,
+            "balance":0
+        }
+
+        r = requests.post("http://localhost:80/users", data=reg)
