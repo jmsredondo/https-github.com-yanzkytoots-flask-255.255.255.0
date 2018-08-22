87374befcef73c1fa9e1f3c7d5634f0f8295e1d0
miac <dcalva120902@gmail.com>
2018-08-22 23:19:28 +0800

    Modified routes and html files

--- app/routes.py
+++ app/routes.py
@@ -25,3 +25,42 @@ def login_page():
             return render_template('Admin/dashboard.html', username=form.username.data)
         return redirect('/login#loginFailed')
     return render_template('login.html', form=form)
+
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    return render_template('Admin/dashboard.html')
+
+
+@app.route('/user', methods=['GET'])
+def user():
+    return render_template('Admin/user.html')
+
+
+@app.route('/genre', methods=['GET'])
+def genre():
+    return render_template('Admin/genre.html')
+
+
+@app.route('/book', methods=['GET'])
+def book():
+    return render_template('Admin/book.html')
+
+
+@app.route('/addgenre', methods=['GET'])
+def addgenre():
+    return render_template('Admin/addgenre.html')
+
+
+@app.route('/addbook', methods=['GET'])
+def addbook():
+    return render_template('Admin/addbook.html')
+
+
+@app.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+
+
+
