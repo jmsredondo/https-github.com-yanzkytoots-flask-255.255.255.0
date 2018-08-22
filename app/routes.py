f54f60064431e1d2ea9a1960a63bb786feb9bd9e
miac <dcalva120902@gmail.com>
2018-08-22 14:31:13 +0800

    Modified routes

--- app/routes.py
+++ app/routes.py
@@ -2,95 +2,34 @@ import requests
 from flask import jsonify, request, render_template, redirect, session, json
 from werkzeug.security import generate_password_hash
 
-
 from app.models import *
 from app import app
+from forms import RegistrationForm
 
 
-@app.route('/signin', methods=['GET', 'POST'])
-def login():
-    if 'username' in session:
-        return redirect('/dashboard')
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        user = User.query.all()
-        for u in user:
-            if u.username == username and u.password == password:
-                session['user'] = username
-                return redirect('/dashboard')
-        return redirect('/signin#loginFailed')
+@app.route('/testing', methods=['GET', 'POST'])
+def testing():
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
 
-    return render_template('login.html')
-
-
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        firstName = request.form.get('firstname')
-        lastName = request.form.get('lastname')
-        phone = request.form.get('phone')
-        email = request.form.get('email')
-        balance = 0
-
-        hashedpassword = generate_password_hash(password)
-
-        new_user = User(username, hashedpassword, firstName, lastName, phone, email,balance)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return redirect('/signin#success')
-
-    return render_template('register.html')
+"""
+def get_testing():
+    form = RegistrationForm()
+    new_dict = {
+        'username': form.username,
+        'password': form.password,
+        'firstname': form.firstname,
+        'lastname': form.lastname,
+        'phone': form.phone,
+        'email': form.email
+    }
+    print "Hello"
+    print new_dict
+    return new_dict
 
 
-@app.route('/dashboard', methods=['POST', 'GET'])
-def dashboard():
-    return render_template('Admin/dashboard.html')
-
-
-@app.route('/user', methods=['GET'])
-def users():
-    all_users = requests.get('http://localhost:80/user').content
-    result = json.loads(all_users)
-    return render_template('Admin/user.html', users=result)
-
-
-@app.route('/book', methods=['GET'])
-def book():
-    all_books = requests.get('http://localhost:80/book').content
-    result = json.loads(all_books)
-    return render_template('Admin/book.html', books=result)
-
-
-@app.route('/', methods=['GET'])
-def logout():
-    return render_template('index.html')
-
-
-@app.route('/shookedbtn', methods=['GET'])
-def shookedbtn():
-    return render_template('Admin/dashboard.html')
-
-
-@app.route('/genre', methods=['GET'])
-def genre_get():
-    all_genres = requests.get('http://localhost:80/genre').content
-    result = json.loads(all_genres)
-    return render_template('Admin/genre.html', genres=result)
-
-
-@app.route('/library', methods=['GET'])
-def libr():
-    return render_template('Admin/libr.html')
-
-
-@app.route('/forgot', methods=['GET'])
-def landing():
-    return render_template('forgot-password.html')
-
-
-
+@app.route('/testing_res', methods=['GET'])
+def res_testing():
+    print "Hi"
+    print get_testing()
+"""
