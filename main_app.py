6f8a0a00a9b02725c918ea766d3b609c6dcbf7d9
jmsredondo <jmspredondo@gmail.com>
2018-10-02 15:36:02 +0800

    edited web

--- main_app.py
+++ main_app.py
@@ -3,4 +3,4 @@ from app import app
 
 if __name__ == '__main__':
     app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-    app.run(debug=True, port=9101)
+    app.run(debug=True, host="0.0.0.0", port=9101)
